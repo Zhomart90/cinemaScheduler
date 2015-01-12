9011b9472078677887afea6d11e97ee9943faa64
Zhomart90 <zhomart.zholdasbekov@metaphor.kz>
2015-01-12 22:44:15 +0600

    my first commit

---
+++ src/main/java/kz/metaphor/controller/MovieController.java
@@ -0,0 +1,39 @@
+package kz.metaphor.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kz.metaphor.model.Movie;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service/user/")
+public class MovieController {
+
+	@RequestMapping( method = RequestMethod.GET,headers="Accept=application/json")
+	public List<Movie> getMovie() {
+		 System.out.println("get movie method is running");
+		 List movies=new ArrayList<Movie>();
+		 Movie movie = new Movie();
+		 movie.setName("Titanic");
+		 movie.setProducer("Cameron");
+		 movies.add(movie);
+		 Movie movie2 = new Movie();
+		 movie2.setName("Troya");
+		 movie2.setProducer("Cameron");
+		 movies.add(movie2);
+		 return movies;
+
+//		model.addAttribute("movie", name);
+//		return "list";
+
+	}
+	
+}
---
+++ src/main/java/kz/metaphor/model/Movie.java
@@ -0,0 +1,22 @@
+package kz.metaphor.model;
+
+public class Movie {
+	
+	private String name;
+	
+	private String producer;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProducer() {
+		return producer;
+	}
+	public void setProducer(String producer) {
+		this.producer = producer;
+	}
+
+}
