505c872961d1328b8bf67483e8944de2af2b970b
Zhomart90 <zhomart.zholdasbekov@metaphor.kz>
2015-01-19 01:01:59 +0600

    ending getMovisByDate method

--- src/main/java/kz/metaphor/controller/MovieController.java
+++ src/main/java/kz/metaphor/controller/MovieController.java
@@ -1,14 +1,9 @@
 package kz.metaphor.controller;
 
-import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
-
 import kz.metaphor.database.DataGetter;
-import kz.metaphor.database.DatabaseConnection;
 import kz.metaphor.model.Movie;
-
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,13 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/movies")
 public class MovieController {
 	
-
 	@RequestMapping( method = RequestMethod.GET,headers="Accept=application/json")
 	public List<Movie> getMovies() {
 		List<Movie> movies=new ArrayList<Movie>();
-		
 		System.out.println("We are in controllers getMovie method");
-		
 		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
 		DataGetter dataGetter= (DataGetter) context.getBean("dataGetter");
 		movies= dataGetter.getMovies();
@@ -37,13 +29,12 @@ public class MovieController {
 	@RequestMapping( method = RequestMethod.GET,headers="Accept=application/json",value="{date}")
 	public List<Movie> getMovieByDate(@PathVariable String date) {
 		List<Movie> movies=new ArrayList<Movie>();
-		
 		System.out.println("We are in controllers getMovieByDate method");
 		System.out.println("date: "+date);
-//		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-//		DataGetter dataGetter= (DataGetter) context.getBean("dataGetter");
-//		movies= dataGetter.getMovies();
-//		System.out.println("movies length:"+movies.size());
+		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+		DataGetter dataGetter= (DataGetter) context.getBean("dataGetter");
+		movies= dataGetter.getMoviesByDate(date);
+		System.out.println("movies length:"+movies.size());
 		return movies;
 	}
 	
--- src/main/java/kz/metaphor/database/DataGetter.java
+++ src/main/java/kz/metaphor/database/DataGetter.java
@@ -20,7 +20,30 @@ public class DataGetter {
 	public List<Movie> getMovies() {
 		List<Movie> movies = new ArrayList<Movie>();
 		String sql = "select * from movies";
+		try {
+			connection = DatabaseConnection.getConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Movie movie = new Movie();
+				movie.setId(rs.getInt("id"));
+				movie.setName(rs.getString("name"));
+				movie.setProducer(rs.getString("producer"));
+				movie.setDate(rs.getDate("date"));
+				movies.add(movie);
+			}
+			rs.close();
+			ps.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return movies;
+	}
 
+	public List<Movie> getMoviesByDate(String date) {
+		List<Movie> movies = new ArrayList<Movie>();
+		String sql = "select * from movies where date = '" + date + "'";
+		System.out.println("sql: " + sql);
 		try {
 			connection = DatabaseConnection.getConnection();
 			PreparedStatement ps = connection.prepareStatement(sql);
